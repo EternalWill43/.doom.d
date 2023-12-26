84712aebf6ddf2e3b726bc64d9c5d6ab31566cb8
eternalwill43 <emilsmoney@live.com>
2023-12-26 03:32:34 -0500

    docs: add date

--- custom.el
+++ custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
- '(org-agenda-files '("~/proj/mpunusuals/TODO.org"))
+ '(org-agenda-files
+   '("~/.doom.d/bugs.org" "/home/dexaim/proj/mpunusuals/TODO.org"))
  '(package-selected-packages '(typescript-mode svelte-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
